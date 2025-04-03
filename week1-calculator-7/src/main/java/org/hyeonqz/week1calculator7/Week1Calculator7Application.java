6cf2899a39ea7617c35d81e99e826366862382ee
hkjin <wlsgusrb78@naver.com>
2025-04-03 23:47:12 +0900

    week1 init

---
+++ week1-calculator-7/src/main/java/org/hyeonqz/week1calculator7/Week1Calculator7Application.java
@@ -0,0 +1,13 @@
+package org.hyeonqz.week1calculator7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1Calculator7Application {
+
+	public static void main (String[] args) {
+		SpringApplication.run(Week1Calculator7Application.class, args);
+	}
+
+}
---
+++ week1-calculator-7/src/test/java/org/hyeonqz/week1calculator7/Week1Calculator7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyeonqz.week1calculator7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1Calculator7ApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
